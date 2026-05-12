6c0c5a31de0a0f12fb59eb48adb181194eb8b12a
bautistv <vbf.bautista@gmail.com>
2026-05-12 23:26:13 +1000

    rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Hallo zusammen",
   description: "made by vyv",
-  openGraph: {
-    images: "/IMG_4024.png",
-  },
 };
 
 export default function RootLayout({
