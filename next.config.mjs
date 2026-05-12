7c5bdb43bb74115253935fd29d93dec5d470e54c
bautistv <vbf.bautista@gmail.com>
2026-05-12 20:38:47 +1000

    fix?

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Tell Next.js to generate static HTML
+  trailingSlash: true,
   images: {
     unoptimized: true, // Required, GH Pages doesn't have an image server
   },
